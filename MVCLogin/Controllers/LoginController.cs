732f14d2ad1e751ab671edba0d660b950509f665
lenovo pc <lenovo pc@LAPTOP-S0IJJ60D>
2020-03-24 19:09:22 +0530

    Add project files.

---
+++ MVCLogin/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using MVCLogin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLogin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Authorise(MVCLogin.Models.tblUser userModel)
+        {
+            using (testdbEntities db = new testdbEntities())
+            {
+                var userDetails = db.tblUsers.Where(x => x.userName == userModel.userName && x.Password == userModel.Password).FirstOrDefault();
+                if (userDetails == null)
+                {
+                    userModel.LoginErrorMessage = "Wrong username or password!";
+                    return View("Index", userModel);
+                }
+                else
+                {
+                    Session["userID"] = userDetails.UserID;
+                    Session["userName"] = userDetails.userName;
+                    return RedirectToAction("Index","Home");
+                }
+            }
+        }
+
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
